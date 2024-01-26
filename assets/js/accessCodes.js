02bc35fa2857c3a29849faa228779942a810ab1c
matthewru <matthewru2005@gmail.com>
2024-01-26 10:18:02 -0600

    access code generation fixed

---
+++ assets/js/accessCodes.js
@@ -0,0 +1,14 @@
+function generateAccessCode() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let accessCode = '';
+
+for (let i = 0; i < 6; i++)
+{
+  const randomIndex = Math.floor(Math.random() * characters.length);
+  accessCode += characters.charAt(randomIndex);
+}
+
+return accessCode;
+}
+
+module.exports = generateAccessCode
