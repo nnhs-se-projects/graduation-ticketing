c103ddc87f406db307d61faf39f5c0fe725e361c
matthewru <matthewru2005@gmail.com>
2024-02-21 11:16:17 -0600

    update database to include unique ticket codes

--- assets/js/initStudentData.js
+++ assets/js/initStudentData.js
@@ -6,14 +6,30 @@ const fs = require("fs")
 const csvFilePath = "StudentData.csv"
 const jsonFilePath = "../../server/model/students.json"
 const csvOptions = {
-  headers: ['id', 'last_name', 'first_name', 'access_code'], // Replace with your desired field names
+  headers: ['id', 'last_name', 'first_name', 'access_code', 'tickets'], // Replace with your desired field names
 };
+
+function generateRandomBarcode() {
+  var randomNumber = Math.floor(Math.random() * 1000000000000); // Random number between 0 and 999,999,999,999
+  var barcodeValue = randomNumber.toString().padStart(12, '0'); // Ensure 12 digits by padding with leading zeros if necessary
+  return barcodeValue;
+}
 
 
 csvtojson(csvOptions).fromFile(csvFilePath).then((obj) => {
   obj.forEach((entry) => {
       entry.access_code = generateAccessCode();
       entry.num_tickets = 2;
+      entry.tickets = [];
+      for (let i = 0; i < entry.num_tickets; i++)
+      {
+        var code = generateRandomBarcode()
+        entry.tickets.push({
+          "barcode" : code,
+          "time_scanned" : null
+        })
+
+      }
     });
   const jsonString = JSON.stringify(obj, null, 2);
   fs.writeFileSync(jsonFilePath, jsonString);
--- server/model/entry.js
+++ server/model/entry.js
@@ -4,6 +4,19 @@
 
 const mongoose = require("mongoose");
 
+const ticketSchema = new mongoose.Schema({
+  barcode: {
+    type: String,
+    required: true,
+  },
+  time_scanned: {
+    type: Date,
+    default: null,
+    required: true,
+  }
+})
+
+
 const schema = new mongoose.Schema({
   id: {
     type: String,
@@ -25,6 +38,7 @@ const schema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  tickets: [ticketSchema]
 });
 
 const Entry = mongoose.model("Entry", schema);
