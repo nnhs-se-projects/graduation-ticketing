59a240edc08714ef4ae1c3ee7de0944bcede6369
matthewru <matthewru2005@gmail.com>
2024-03-08 20:05:55 -0600

    connected receiver to database

--- recievescan/receivebarcodeserver.js
+++ recievescan/receivebarcodeserver.js
@@ -5,43 +5,49 @@
 
 // import the express module, which exports the express function
 const express = require("express");
+const entry = require("../server/model/entry")
 // invoke the express function to create an Express application
 const app = express();
 
-// import the express-session module, which is used to manage sessions
-// const session = require("express-session");
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );aaa
+
 
 const dotenv = require("dotenv");
-dotenv.config({ path: ".env" });
+dotenv.config({ path: "../.env" });
 
-// add middleware to handle JSON in HTTP request bodies (used with POST commands)
+const connectDB = require("../server/database/connection");
+connectDB();
 app.use(express.json());
 
-// set the template engine to EJS, which generates HTML with embedded JavaScript
 app.set("view engine", "ejs");
 
-// load assets
 app.use("/css", express.static("assets/css"));
 app.use("/img", express.static("assets/img"));
 app.use("/js", express.static("assets/js"));
 
-
-// to keep this file manageable, we will move the routes to a separate file
-//  the exported router object is an example of middleware
 app.get("/", async (req, res) => {
-  // the req parameter references the HTTP request object, which has a number
-  //  of properties
   console.log("path requested: " + req.path);
-  // the res parameter references the HTTP response object
   res.render("receiver");
 });
+
+app.post("/scanned", (req, res) => {
+  const barcode = req.body.barcode;
+  const time_scanned = req.body.timestamp;
+  studentObj = {}
+  entry.findOne({ 'tickets.barcode': barcode})
+    .then(student => {
+      if (!student) {
+        return res.status(404).json({ message: 'Student not found' });
+      }
+      
+      res.json(student)
+    })
+    .catch(error => {
+      console.error('Error searching for student:', error);
+    })
+  console.log('Received data: ' + barcode + " at " + time_scanned);
+
+})
+
 
 
 // start the server on port 8080
